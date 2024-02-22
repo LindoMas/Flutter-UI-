b5b784b3a3006cd977d0410c9c9c84721ffee1d5
lmashele <lmashele023b@student.wethinkcode.co.za>
2024-02-22 15:50:26 +0200

    day 2

--- Inspiration App UI & Animation day-2/inspirationapp/lib/main.dart
+++ Inspiration App UI & Animation day-2/inspirationapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(
       const MaterialApp(
@@ -24,84 +25,188 @@ class MainApp extends StatelessWidget {
             )),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-              padding: EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                  borderRadius:
-                      BorderRadius.vertical(bottom: Radius.circular(20)),
-                  color: Colors.white),
-              child: Column(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(20.0),
+                decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.vertical(bottom: Radius.circular(20)),
+                    color: Colors.white),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Be Inspired with",
+                      style: TextStyle(color: Colors.black87, fontSize: 28),
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      "ArtInk",
+                      style: TextStyle(color: Colors.black, fontSize: 40),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                          color: Color.fromRGBO(244, 234, 254, 1),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10),
+                        child: TextField(
+                          decoration: InputDecoration(
+                              prefixIcon: Icon(Icons.search),
+                              hintText: "What intrests you?",
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 28.0),
+                child: Column(
+                  children: [
+                    Text(
+                      "Tending Today",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20),
+                height: 200,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    SampleCard("assets/pic0.jpg", 1),
+                    SampleCard("assets/pic1.jpg", 2),
+                    SampleCard("assets/pic8.jpg", 3),
+                    SampleCard("assets/pic3.jpg", 4),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Be Inspired with",
-                    style: TextStyle(color: Colors.black87, fontSize: 28),
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Text(
-                    "ArtInk",
-                    style: TextStyle(color: Colors.black, fontSize: 40),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20.0, bottom: 8),
+                    child: Text(
+                      "Most Viewed",
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
                   ),
                   Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    height: 150,
                     decoration: BoxDecoration(
-                        color: Color.fromRGBO(244, 234, 254, 1),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 10),
-                      child: TextField(
-                        decoration: InputDecoration(
-                            prefixIcon: Icon(Icons.search),
-                            hintText: "What intrests you?",
-                            border: InputBorder.none),
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: AssetImage("assets/pic4.jpg"),
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [
+                                .2,
+                                .9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.1)
+                              ])),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+                        child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              "Art by : 暗部的黄",
+                              style: GoogleFonts.aboreto(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            )),
                       ),
                     ),
                   ),
                   SizedBox(
-                    height: 20,
-                  )
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 28.0),
-              child: Column(
-                children: [
-                  Text(
-                    "Today's Promo",
-                    style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    height: 10,
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 20),
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("assets/pic7.jpg"),
+                      ),
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              stops: [
+                                .2,
+                                .9
+                              ],
+                              colors: [
+                                Colors.black.withOpacity(.8),
+                                Colors.black.withOpacity(.1)
+                              ])),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+                        child: Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              "Art by : 暗部的黄",
+                              style: GoogleFonts.aboreto(
+                                  fontSize: 19,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            )),
+                      ),
+                    ),
                   ),
                 ],
-              ),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            Container(
-              height: 200,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[SampleCard("assets/pic0.jpg")],
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget SampleCard(image) {
+  Widget SampleCard(image, index) {
     return AspectRatio(
       aspectRatio: 2 / 3,
       child: Container(
+        margin: EdgeInsets.only(right: 16),
         height: 100,
         width: 60,
         decoration: BoxDecoration(
@@ -121,6 +226,18 @@ class MainApp extends StatelessWidget {
                 Colors.black.withOpacity(.8),
                 Colors.black.withOpacity(.1)
               ])),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+            child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  "# " + index.toString(),
+                  style: GoogleFonts.aboreto(
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                )),
+          ),
         ),
       ),
     );
